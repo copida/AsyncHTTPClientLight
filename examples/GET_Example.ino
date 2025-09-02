1a1bd0e91487226c0b8cd34e9aa6358556977b47
copida <copida.soft@gmail.com>
2025-09-02 14:07:47 +0200

    Update GET_Example.ino

--- examples/GET_Example.ino
+++ examples/GET_Example.ino
@@ -9,24 +9,58 @@ AsyncHTTPClientLight client;
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) delay(500);
-  Serial.println("WiFi connesso");
+  Serial.print("Connessione WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connesso!");
+  Serial.println(WiFi.localIP());
 
-  client.setDebug(true); // Attiva il debug (solo se ASYNC_HTTP_DEBUG è definito)
+  // Configurazione client
+  client.setDebug(true);
+  client.setLogToFile(true);
+  client.setMaxRetries(3);
+  client.setTimeout(8000);
 
-  client.onRequestComplete([](int code, String body) {
-    Serial.println("Risposta GET ricevuta:");
-    Serial.println("Codice: " + String(code));
-    Serial.println("Body:\n" + body);
-  });
+  // Callback unificato
+  client.onEvent([](HTTPEventType type, const String& msg) {
+  switch (type) {
+    case HTTPEventType::Response:
+      Serial.println("Risposta ricevuta: " + msg);
+      Serial.println("Codice HTTP: " + String(client.getLastHTTPcode()));
+      break;
+    case HTTPEventType::Timeout:
+      Serial.println("Timeout: " + msg);
+      break;
+    case HTTPEventType::Error:
+      Serial.println("Errore: " + msg);
+      break;
+    case HTTPEventType::Chunk:
+      Serial.println("Chunk ricevuto: " + msg);
+      break;
+    case HTTPEventType::Overload:
+      Serial.println("Richiesta ignorata: " + msg);
+      break;
+  }
+});
 
-  client.onError([](String err) {
-    Serial.println("Errore: " + err);
-  });
 
-  client.beginRequest("https://jsonplaceholder.typicode.com/posts/1", "GET");
+  // Header personalizzati
+  client.addHeader("Authorization", "Bearer xyz123");
+  client.addHeader("Custom-Header", "DavidePower");
+
+  // Titolo per logging
+  client.addTitle("Richiesta GET iniziale");
+
+  // Avvio richiesta asincrona
+  client.beginRequest("http://httpbin.org/get", "GET", "");
 }
 
 void loop() {
   client.poll();
+  if (client.isFinished()) {
+    Serial.println("Risposta ricevuta:");
+    Serial.println(client.getLastResponseBody());
+  }
 }
